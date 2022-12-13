5a30a9c4251ae269cb35b97295fbc9186aa56df1
PQ <3242873590@qq.com>
2022-12-13 22:10:24 +0800

    feat: 首页支持中文

--- src/assets/lang/index.ts
+++ src/assets/lang/index.ts
@@ -4,7 +4,7 @@ import en from './en.ts';
 
 const i18n = createI18n({
     globalInjection: true,
-    locale: 'en',
+    locale: 'zh',
     messages: {
         zh: zh,
         en: en
--- src/assets/lang/zh.ts
+++ src/assets/lang/zh.ts
@@ -1,6 +1,12 @@
 export default {
     welcome: {
         title: 'Glassmorphism Generator',
-        desc: '这是一个"玻璃拟态"CSS生成器，用于帮助设计师、前端开发者来快速设计组件。'
+        desc: '这是一个"玻璃拟态"CSS生成器，用于帮助设计师、前端开发者来快速设计组件。',
+        start:"开始"
+    },
+    nav:{
+        start :"开始",
+        download:"下载",
+        about:"关于"
     }
 }
